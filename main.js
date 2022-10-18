3b8e1e859794a4b40df51613d335fdafc0580e23
Sima <tarverdans15@gmail.com>
2022-10-18 19:48:12 +0300

    git commit -am

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const startBtn = document.querySelector('.startBtn');
+const StartBtn = document.querySelector('.StartBtn');
 const mainStartBtn = document.querySelector('.mainLapBtn');
 
-const lapResetBtn = document.querySelector('.lapResetBtn');
+const LapResetBtn = document.querySelector('.LapResetBtn');
 const mainLapBtn = document.querySelector('.mainLapBtn');
 
 const laps = document.querySelector('.laps');
@@ -19,30 +19,99 @@ let int = null;
 
 let isTimerStarted = false;
 
-startBtn.addEventListener("click", () => {
-if(isTimerStarted === false) {
-int = setInterval(displayTimer, 10);
-} else {
-clearInterval(int)
-}
+let allLaps = [];
+let lapsNumber = 1;
+
+StartBtn.addEventListener('click', function () {
+    if(isTimerStarted === false) {
+        int = setInterval(displayTimer, 10);
+    } else {
+        clearInterval(int);
+    }
+    changeStartBtn();
+    checkIsResetAvailable();
 })
 
 
+
 function displayTimer() {
-milisec++;
+    milisec++;
 
-if(milisec >= 99){
-sec++;
-milisec = 0;
-}
-if(sec >= 60){
-min++;
-sec = 0;
-}
-m = min < 10 ? "0" + min : min;
-s = sec < 10 ? "0" + sec : sec;
-ms = milisec < 10 ? "0" + milisec : milisec;
+    if (milisec >= 99) {
+        sec++;
+        milisec = 0;
+    }
+    if (sec >= 60) {
+        min++;
+        sec = 0;
+    }
 
-time.innerHTML = `${m}:${s},${ms}`;
+    m = min < 10 ? "0" + min : min;
+    s = sec < 10 ? "0" + sec : sec;
+    ms = milisec < 10 ? "0" + milisec : milisec;
+
+    time.innerHTML = `${m}:${s},${ms}`;
 
 }
+
+function changeStartBtn() {
+    if (isTimerStarted === false) {
+        isTimerStarted = true;
+
+        StartBtn.innerHTML = 'Stop'
+        StartBtn.classList.add('timerStarted');
+        mainStartBtn.classList.add('timerStartedMain');
+    } else {
+        isTimerStarted = false;
+
+        StartBtn.innerHTML = 'Start'
+        StartBtn.classList.remove('timerStarted');
+        mainStartBtn.classList.remove('timerStartedMain');
+    }
+}
+
+function checkIsResetAvailable(){
+    if(isTimerStarted === false) {
+        LapResetBtn.innerHTML = 'Reset';
+    } else {
+        LapResetBtn.innerHTML = 'Lap'
+    }
+}
+
+function displayLaps() {
+    lapsNumber++;
+    laps.innerHTML = ''
+    if(allLaps.length > 0) {
+        allLaps.map(item => {
+            laps.innerHTML += `
+                <div class='lap'>
+                    <span>Lap ${item.number}</span>
+                    <span>${item.time}</span>
+                </div>
+            `
+        })
+    }
+}
+LapResetBtn.addEventListener('click', () => {
+    if(isTimerStarted === false) { // reset click
+        clearInterval(int);
+        m = 0;
+        s = 0;
+        ms = 0;
+
+        milisec = 0;
+        sec = 0;
+        min = 0;
+        time.innerHTML = '00:00,00';
+    } else { //lap click
+        allLaps.push(
+            {
+                time: m + ':' + s + ',' + ms,
+                number: lapsNumber,
+            }
+        )
+
+        displayLaps();
+        console.log(allLaps);
+    }
+})
